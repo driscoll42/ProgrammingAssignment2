3b4d50db1ed020efd372f1c5df2c1c2f56f98fed
Michael Driscoll <driscoll42@gmail.com>
2014-04-27 18:53:58 -0500

    Added code to cache Matrix
    Mostly just a copy/paste of the vector one replacing mean with solve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion is usually a costly computation and their may be 
+## some benefit to caching the inverse of a matrix rather than compute
+## it repeatedly (there are also alternatives to matrix inversion that 
+## we will not discuss here). Your assignment is to write a pair of
+## functions that cache the inverse of a matrix.
 
-## Write a short comment describing this function
+## Computing the inverse of a square matrix can be done with the solve 
+## function in R. For example, if X is a square invertible matrix, then
+## solve(X) returns its inverse.
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## This function creates a special "matrix" object that can cache its
+  ## inverse.
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) m <<- solve
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
+  
 }
 
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## This function computes the inverse of the special "matrix" returned
+  ## by makeCacheMatrix above. If the inverse has already been calculated
+  ## (and the matrix has not changed), then the cachesolve should retrieve 
+  ## the inverse from the cache.
+  m <- x$getinv()                 #query the x matrix's cache         
+  if(!is.null(m)) {               #if there is a cache
+    message("getting cached data") 
+    return(m)                     #just return the cache, no computation needed
+  }
+  data <- x$get()                 #if there's no cache
+  m <- solve(data, ...)           #we actually compute them here
+  x$setinv(m)                     #save the result back to x's cache
+  m                               #return the result
 }
